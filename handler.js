5f15ad51f1c9153e12e00ba018437beed935a512
mjvalerio <valerio.matt@gmail.com>
2017-06-26 18:36:29 -0400

    added the ability to intercom everyone

--- handler.js
+++ handler.js
@@ -59,7 +59,13 @@ module.exports.message = (event, context, callback) => {
   console.log("sonosZone - " + sonosZone);
 
   console.log("ready to call sonos api");
-  axios.get(`${SONOS_API_SERVER}/${sonosZone}/say/${messageContent}`, options)
+  //axios.get(`${SONOS_API_SERVER}/${sonosZone}/pause`);
+  if (sonosZone.toLowerCase() == 'everyone' || sonosZone.toLowerCase() == 'all') {
+    var sonosURL = `${SONOS_API_SERVER}/sayall/${messageContent}`;
+  } else {
+    var sonosURL = `${SONOS_API_SERVER}/${sonosZone}/say/${messageContent}`
+  }
+  axios.get(sonosURL, options)
       .then(response => {
           console.log("SUCCESS: broadcasted message.");
           callback(null, message(
@@ -75,4 +81,5 @@ module.exports.message = (event, context, callback) => {
             "Check error logs for more information"
           ))
       });
+  //axios.get(`${SONOS_API_SERVER}/${sonosZone}/play`);
 }
